55ceb073cd5b5fd84ca31ab92a18ff40c4a8aeda
Kaio <91427326+Kaicius@users.noreply.github.com>
2026-06-08 09:52:27 -0300

    Update esqueci-senha.component.ts

--- src/app/pages/publico/esqueci-senha/esqueci-senha.component.ts
+++ src/app/pages/publico/esqueci-senha/esqueci-senha.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-esqueci-senha',
@@ -12,24 +14,72 @@ import { Router, RouterLink } from '@angular/router';
 })
 export class EsqueciSenhaComponent {
   email = '';
+  codigoOtp = '';
+  enviouCodigo = false;
+
   erro = '';
   sucesso = '';
+  carregando = false;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private http: HttpClient) {}
 
   enviarLink() {
     this.erro = '';
     this.sucesso = '';
 
-    if (!this.email) {
-      this.erro = 'Digite seu e-mail.';
+    if (!this.email.trim()) {
+      this.erro = 'Por favor, digite seu e-mail.';
       return;
     }
 
-    this.sucesso = 'Link enviado! Redirecionando...';
+    this.carregando = true;
 
-    setTimeout(() => {
-      this.router.navigate(['/usuario/reset-senha']);
-    }, 1200);
+    this.http.post(`${environment.apiUrl}/user/login/esqueci-a-senha`, { email: this.email.trim().toLowerCase() })
+      .subscribe({
+        next: (response: any) => {
+          this.carregando = false;
+          this.enviouCodigo = true;
+          this.sucesso = 'Código de verificação enviado para o seu e-mail!';
+        },
+        error: (err) => {
+          this.carregando = false;
+          console.error(err);
+          this.erro = err.error?.mensagem || 'E-mail não encontrado ou erro no servidor.';
+        }
+      });
+  }
+
+  verificarCodigo() {
+    this.erro = '';
+    this.sucesso = '';
+
+    if (!this.codigoOtp.trim()) {
+      this.erro = 'Informe o código recebido por e-mail.';
+      return;
+    }
+
+    this.carregando = true;
+
+    this.http.post(`${environment.apiUrl}/user/login/esqueci-a-senha-confirmar`, { codigo: this.codigoOtp.trim() }, { withCredentials: true })
+      .subscribe({
+        next: (response: any) => {
+          this.carregando = false;
+          this.sucesso = 'Código validado! Redirecionando...';
+          
+
+          if (response && response.token_reset) {
+            localStorage.setItem('tokenResetJWT', response.token_reset);
+          }
+
+          setTimeout(() => {
+            this.router.navigate(['/reset-senha']);
+          }, 1200);
+        },
+        error: (err) => {
+          this.carregando = false;
+          console.error(err);
+          this.erro = err.error?.mensagem || 'Código inválido ou expirado.';
+        }
+      });
   }
 }
